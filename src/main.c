98a53c5337b4ade313e27cff7c58a34ada96cd0e
Michael Mierzwa <entwickler.mit.bart@gmail.com>
2025-09-08 23:01:57 +0200

    Add Employee scheint zu funktionieren.

--- src/main.c
+++ src/main.c
@@ -11,6 +11,7 @@ void print_usage(char *argv[]) {
     printf("Usage: %s -n -f <database file>\n", argv[0]);
     printf("\t -n   - create new database file\n");
     printf("\t -f   - (required) path to database file\n");
+    printf("\t -a   - add a new employee\n");
     return;
 }
 
@@ -18,17 +19,22 @@ int main(int argc, char *argv[]) {
     int c;
     bool newfile = false;
     char *filepath = NULL;
+    char *addstring = NULL;
     int dbfd = -1;
 
     struct dbheader_t *dbhdr = NULL;
+    struct employee_t *employees = NULL;
     
-    while ((c = getopt(argc, argv, "nf:")) != -1) {
+    while ((c = getopt(argc, argv, "nf:a:")) != -1) {
         switch (c) {
             case 'n':
                 newfile = true;
                 break;
             case 'f':
                 filepath = optarg;
+                break;
+            case 'a':
+                addstring = optarg;
                 break;
             case '?':
                 printf("Unknown option - %c\n", c);
@@ -68,7 +74,18 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    output_file(dbfd, dbhdr);
+    if (read_employees(dbfd, dbhdr, &employees) == STATUS_ERROR) {
+        printf("Failed to read employees\n");
+        return -1;
+    }
+
+    if (addstring) {
+        dbhdr->count++;
+        employees = realloc(employees, dbhdr->count * sizeof(struct employee_t));
+        add_employee(dbhdr, employees, addstring);
+    }
+
+    output_file(dbfd, dbhdr, employees);
     
     free(dbhdr);
     return 0;
